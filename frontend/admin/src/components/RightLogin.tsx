209df7d6edd190ad0dbcc1e4fd2f153dc2315ab2
Datto <dattotienn@gmail.com>
2025-05-14 03:58:26 +0700

    Login scence

--- frontend/admin/src/components/RightLogin.tsx
+++ frontend/admin/src/components/RightLogin.tsx
@@ -1,24 +1,37 @@
-import { Input, Button, Form } from 'antd';
+import { Input, Button, Form } from "antd";
+import { Card, Checkbox } from "antd";
+import "./RightLogin.css";
 function right_login() {
-    return (
-        <div className="right-panel">
-        <div className="login-box">
-          <Form layout="vertical">
-            <Form.Item>
-              <Input placeholder="Tài khoản" className="input" />
-            </Form.Item>
-            <Form.Item>
-              <Input.Password placeholder="Mật khẩu" className="input" />
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit" className="login-btn">
-                Đăng nhập
-              </Button>
-            </Form.Item>
-          </Form>
-        </div>
-      </div>
-    );
+  return (
+    <div className="r_box">
+      <Card className="r_card">
+        <Form name="basic" className="login-form">
+          <h1 className="login-title">Đăng nhập hệ thống</h1>
+          <Form.Item
+            label="Tên đăng nhập"
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Mật khẩu"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked" label={null}>
+            <Checkbox>Nhớ mật khẩu</Checkbox>
+          </Form.Item>
+          <Form.Item label={null}>
+            <Button className="login-button" type="primary" htmlType="submit">
+              Đăng nhập
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 }
 export default right_login;
- 
