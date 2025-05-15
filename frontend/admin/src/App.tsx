533bd8e1d3a4e813c2e5ad6fcd1fedf1dafb4cae
Datto <dattotienn@gmail.com>
2025-05-15 19:46:02 +0700

    Save

--- frontend/admin/src/App.tsx
+++ frontend/admin/src/App.tsx
@@ -1,11 +1,24 @@
 import "./App.css";
 import LoginPage from "./page/LoginPage.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AdminPage from "./page/AdminPage.tsx";
+import Settings from "./page/Settings.tsx";
+import Attendance from "./page/Attendance.tsx";
+import StudentManagement from "./page/StudentManagement.tsx";
 function App() {
   return (
     <>
-      <LoginPage></LoginPage>
+      <BrowserRouter>
+        <AdminPage />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/attendance" element={<Attendance />} />
+          <Route path="/students" element={<StudentManagement />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App;
