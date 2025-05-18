39a79e24e29a69a01141075fb230766b510e2f31
minh071289 <dangminhnguyet2015@gmail.com>
2025-05-18 22:54:01 +0700

    Update model.py

--- backend/service/model.py
+++ backend/service/model.py
@@ -1,6 +1,7 @@
 # service.model
 import torch
 import cv2
+import pymongo
 from facenet_pytorch import MTCNN, InceptionResnetV1
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -11,35 +12,23 @@ mtcnn = MTCNN(keep_all=True, device=device)
 # Model ResNet -> embedding faces
 resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
 
-# Database tam
-known_faces = {
-    # "cin cit": "D:\\23020407 Dang Minh Nguyet\\5in1\\database\\nguyet.png",
-    # "minh ly": "D:\\23020407 Dang Minh Nguyet\\5in1\\database\\li.png",
-    # "datto": "D:\\23020407 Dang Minh Nguyet\\5in1\\database\\datto.jpg",
-    # "panh": "D:\\23020407 Dang Minh Nguyet\\5in1\\database\\cpa.jpg",
-    # "Nguyen Thi Phuong": "D:\\23020407 Dang Minh Nguyet\\5in1\\database\\phuong.png"
-}
 
-def encode_known_faces(known_faces: dict):
-    encodings = {}
+def load_encoding_from_students():
+    client = pymongo.MongoClient("mongodb+srv://dangminhnguyet2015:mongodb@cluster0.srvjgt8.mongodb.net/") 
+    db = client["Attendances"]
+    collection = db["students"]
+    students = collection.find({})
+    encoding_dict = {}
 
-    for name, img_path in known_faces.items():
-        img = cv2.imread(img_path)
-        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        faces = mtcnn(img_rgb)
+    for student in students:
+        name = student.get("full_name")
+        embeddings = student.get("image_encoding")
 
-        if faces is not None:
-            for face in faces:
-                embedding = resnet(face.unsqueeze(0).to(device)).detach().cpu().numpy().flatten()
-                if name not in encodings:
-                    encodings[name] = []
-                encodings[name].append(embedding)
-        else:
-            print(f"[WARN] Không phát hiện khuôn mặt trong ảnh: {img_path}")
+        if not name or not embeddings:
+            continue
 
-    return encodings
+        # Convert all embeddings back to np.array or keep as list
+        encoding_dict[name] = [np.array(emb) for emb in embeddings]
 
-# known_faces = encode_known_faces(known_faces)
-known_faces = {}
-for name, emb_list in known_faces.items():
-    print(f"{name}: {type(emb_list)}, length = {len(emb_list)}")
+    print(f"[INFO] Loaded encodings for {len(encoding_dict)} students.")
+    return encoding_dict
